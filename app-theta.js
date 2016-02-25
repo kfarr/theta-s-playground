b88a577cb1293e320f15c5c0c9008bd5cfd30a4a
kfarr <kieran.farr@gmail.com>
2016-02-24 22:29:38 -0800

    working prototype, not pretty code

--- app-theta.js
+++ app-theta.js
@@ -1,69 +1,96 @@
 var fs = require('fs');
-var ThetaSOscClientClass = require('osc-client-theta_s').ThetaSOscClient;
+var ThetaSOscClient = require('osc-client-theta_s').ThetaSOscClient;
 
-// http://192.168.1.1:80/osc/commands/execute
 var domain = '192.168.1.1';
 var port = '80';
-// var client = new OscClientClass(domain, port);
-// var sessionId;
-var filename;
 
-console.log("------------ STARTING METHOD 1 - _thetaClient API");
-
-
-
-
-var _thetaClient = new ThetaSOscClientClass();
-var _sessionId;
+var thetaClient = new ThetaSOscClient(domain, port);
 var sessionId;
+var fileName;
 
-// 1. Get session-id
-_thetaClient.startSession().then(function(res){
-  sessionId = res.body.results.sessionId;
-  // 2. Change captureMode to "_video"
-  return _thetaClient.setOptions(_sessionId, {captureMode:"_video"})
-}).then(function(res){
-  // 3. Start capturing
-  return _thetaClient.listAll({entryCount:1, sort:"newest"});
-}).catch(function (error) {
-  console.log(error);
-});
+// module.exports = function (socket) {
 
+  thetaClient.startSession().then(function (res) {
+    //Grab the session id
+    sessionId = res.body.results.sessionId;
+    console.log('set options on theta client');
+    return thetaClient.setOptions(sessionId, {captureMode:"image"})
+  })
+  .then(function (res) {
+    console.log('starting capture');
+    return thetaClient.takePicture(sessionId);
+//    return thetaClient.startCapture(sessionId);
 
-/*
-// 1. Stop capturing
-_thetaClient.stopCapture(_sessionId)
-.then(function(res){
-  // 2. Get list of contents in a device
-  return _thetaClient.listAll({entryCount:1, sort:"newest"});
-}).then(function(res){
-  // 3. Download a movie
-  return _thetaClient.getVideo(res.body.results.entries[0], "full");
-}).then(function(res){
-  // 4. Store the movie
-  fs.writeFile(filename, res.body);
-}).then(function(err){
-  // 5. Close session
-  return client.closeSession(sessionId);
-}).catch(function (error) {
-  console.log(error);
-});
-*/
-console.log("------------ STARTING METHOD 2 - request lib");
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-var request = require('request');
+  // Delay for camera to write image -- #TODO there has got to be a better way to do this
+  var interval = 8000;
+  // Number of keyframes to grab
+  var count = 3;
 
-request.post(
-  'http://192.168.1.1:80/osc/commands/execute',
-  { form: {"name": "camera._listAll",
-      "parameters": {
-      "entryCount": 5,
-      "maxSize": 10,
-      "includeThumb": false
-      }
-  }},
-  function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
+  setTimeout(function () {
+    Promise.resolve()
+//    console.log('stopping capture')/
+//    thetaClient.stopCapture(sessionId)
+    .then(function (_res) {
+      console.log('running listall');
+      // need to get the unique video uri
+      return thetaClient.listAll({entryCount:1, sort:"newest"});
+    })
+    .then(function (res) {
+      fileName = res.body.results.entries[0].name;
+      console.log('res.body.results.entries' + res.body.results.entries);
+      console.log('running getimage on:');
+      console.log(fileName);
+      return thetaClient.getImage(res.body.results.entries[0].uri, "full");
+    })
+    .then(function (res) {
+      console.log('writing file called:');
+      console.log(fileName);
+      fs.writeFile(fileName, res.body);
+      //somehow emit the data through the socket to front end
+    })
+    .then(function (err) {
+      //don't close session yet
+      //call python script with the fileName
+      console.log('closing the session');
+      return thetaClient.closeSession(sessionId);
+    })
+    // clip keyframes and save them
+    .then(function () {
+      getFrames(fileName, count);
+      setTimeout(function () {
+        Emotion('/Users/natelevine/DevWeek16/Hackathon/Gauger/tn_1.png',
+        function (data) {
+          console.log('1: ', data);
+          // socket.emit('dataUpdate', data);
+        });
+        Emotion('/Users/natelevine/DevWeek16/Hackathon/Gauger/tn_2.png',
+        function (data) {
+          console.log('2: ', data);
+          // socket.emit('dataUpdate', data);
+        });
+        Emotion('/Users/natelevine/DevWeek16/Hackathon/Gauger/tn_3.png',
+        function (data) {
+          console.log('3: ', data);
+          // socket.emit('dataUpdate', data);
+        });
+      }, 5000)
+    })
+    //then for each keyframe (3), call the emotion module w/ callback
+    // .then(function () {
+    //
+    // })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }, interval);
+// };
+
+// function analyzeEmotion () {
+//   //callback: take the numbers and emit them through the socket to the front end
+//   Emotion('../tn_2.png', function (data) {console.log(data);});
+// }
